18caf2a30a2ef49661b6fed51f325a6d6152774e
checorazza <ceciicorazza@gmail.com>
2026-07-18 14:06:07 -0300

    sqlalchemy config

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Configuración de SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
