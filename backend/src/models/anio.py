ebcf2b11dac265cdeab3b4f758c62598a5428790
checorazza <ceciicorazza@gmail.com>
2026-07-23 12:11:56 -0300

    fix typos

--- backend/src/models/anio.py
+++ backend/src/models/anio.py
@@ -1,15 +1,18 @@
 from . import db
 
-class Carrera(db.Model):
-    __tablename__ = 'Anio'
+class Anio(db.Model):
+    __tablename__ = 'anio'
     id_anio = db.Column(db.Integer, primary_key=True)
     numero = db.Column(db.Integer, nullable=False)
 
     # Relaciones
     id_carrera = db.Column(db.Integer, db.ForeignKey('carrera.id_carrera'), nullable=False)
 
+    # backref a materia
+    materias = db.relationship("Materia", backref="anio", lazy=True)
+
     def to_json(self):
         return {
-            'id_carrera': self.id_carrera,
-            'nombre': self.nombre,
-        }
+            'id_anio': self.id_anio, 
+            'numero': self.numero, 
+            'id_carrera': self.id_carrera}
--- backend/src/models/carrera.py
+++ backend/src/models/carrera.py
@@ -1,12 +1,12 @@
 from . import db
 
 class Carrera(db.Model):
-    __tablename__ = 'Carrera'
+    __tablename__ = 'carrera'
     id_carrera = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
 
     #backref a año
-    anios = db.relationship("Anio", backref="anios", lazy = True)
+    anios = db.relationship("Anio", backref="carrera", lazy=True)
 
     def to_json(self):
         return {
